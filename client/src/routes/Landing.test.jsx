65717f19142a6f333260dac53c4ef58d3c084b49
vostersc <vosters01@gmail.com>
2024-03-18 13:29:07 -0600

    adding new test f

---
+++ client/src/routes/Landing.test.jsx
@@ -0,0 +1,26 @@
+// import * as router from 'react-router-dom'
+
+import { describe, expect, it } from 'vitest'
+// import { render, screen } from '@testing-library/react';
+
+// import Landing from './Landing';
+
+// vi.mock(`react-router-dom`, async () => ({ ...(await vi.importActual(`react-router-dom`)), useNavigate: ()=> vi.fn() } ));
+// vi.mock(`react-router-dom`, async () => ({ ...(await vi.importActual(`react-redux`)), useSelector: ()=> vi.fn() } ));
+// vi.mock(`react-router-dom`, async () => ({ ...(await vi.importActual(`react-router-dom`)), Link: ()=> vi.fn() } ));
+
+describe('A truthy statement', () => {
+    // beforeEach(() => {
+    //     expect(router);
+    // })
+
+    it('should be equal to 2', () => {
+        expect(1+1).toEqual(2)
+    })
+
+
+
+    // render(<Landing/>);
+    
+    // screen.debug();
+})
